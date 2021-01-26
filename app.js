ba6cd42e377591cd1893747bf73f45bb9b285d35
Pavel <verccety1@gmail.com>
2021-01-26 22:49:25 +0700

    Handling GET requests

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// app.get('/', (req,res) => {
+//    res.status(200).json({message: 'Hello from the server side', app: 'Natours'})
+// })
+
+// app.post('/', (req,res) => {
+//   res.send('You can post to this endpoint..')
+// })
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length, // best practice, not necessary, for mult. obj in array
+    data: {
+      tours: tours
+    }
+    
+  })
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
