341b3e01117cd0b32602a53d0f3f76d81e969a6c
Pavel <verccety1@gmail.com>
2021-01-27 15:12:55 +0700

    Create multiple routers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toUTCString();
@@ -95,15 +95,51 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 // 3) ROUTES
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
+tourRouter.route('/').get(getAllTours).post(createTour);
 
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 // 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
