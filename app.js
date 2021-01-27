bc4f0687183c50f55a7b3ec41925e88cb5b52662
Pavel <verccety1@gmail.com>
2021-01-27 12:34:58 +0700

    Handling DELETE requests with placeholder

--- app.js
+++ app.js
@@ -58,6 +58,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1 // умножим на 1 для конвертации в число из строки
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      messaage: 'Invalid ID'
+    })
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
