63f245d4127a377bd91b823a65acf021912dbfd4
Pavel <verccety1@gmail.com>
2021-01-31 12:01:00 +0700

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -87,6 +87,12 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+//Aggregation Middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
