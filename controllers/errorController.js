0fec0b94159fb3c9046de90ce88dde2e09e78283
Pavel <verccety1@gmail.com>
2021-02-01 22:36:06 +0700

    Differentiate error handling in production or dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,41 @@
+/* eslint-disable no-console */
+const sendErrorDevelopment = (error, response) => {
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error,
+  });
+};
+
+const sendErrorProduction = (error, response) => {
+  // Operational, trusted error: send message to the client
+  if (error.isOperational) {
+    response.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+    // Programming or other unknown error: don't send error details
+  } else {
+    //1) Log error
+    console.error('ERROR', error);
+
+    //2) Send generic message
+    response.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (error, request, response, next) => {
   // By specifying four parameters, Express automatically knows that entire function is an error handling middleware
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
-  response.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDevelopment(error, response);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(error, response);
+  }
 };
