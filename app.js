1ccadc9e9f65b0a0c5b8d80a4410a4effd9a7736
Pavel <verccety1@gmail.com>
2021-02-01 15:08:52 +0700

    Implement global error handling middleware

--- app.js
+++ app.js
@@ -23,11 +23,28 @@ app.use((request, response, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (request, response) => {
-  response.status(404).json({
-    status: 'fail',
-    message: `Can't find ${request.originalUrl} on this server!`,
+app.all('*', (request, response, next) => {
+  // response.status(404).json({status: 'fail', message: `Can't find
+  //   ${request.originalUrl} on this server!`,
+  // });
+
+  const error = new Error(`Can't find ${request.originalUrl} on this server!`);
+  error.status = 'fail';
+  error.statusCode = 404;
+  next(error); // If the (next) function receives  an argument,
+  //Express will automatically know that there was an error; applies to
+  //every next function in every single middleware anywhere
+});
+
+app.use((error, request, response, next) => {
+  // By specifying four parameters, Express automatically knows that entire function is an error handling middleware
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 });
+
 // 4) START SERVER
 module.exports = app;
