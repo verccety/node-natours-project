a52ab0c0b458b91e11018d561cb84b09b860fbee
Pavel <verccety1@gmail.com>
2021-02-02 19:24:21 +0700

    Handle uncaught exceptions/rejections

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+/* eslint-disable no-console */
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+//Exception handling should be at the top
+process.on('uncaughtException', (error) => {
+  console.log('Unhandled exception, shutting down..');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 
@@ -19,7 +27,14 @@ mongoose
   .then(() => {});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+// Listening to unhandled rejection event => allows to handle all the errors that occur in asynchronous code which were not previously handled.
+process.on('unhandledRejection', (error) => {
+  console.log('Unhandled rejection. Shutting down..', error.message);
+  //Shutdown gracefully: first close the server, then shut down the app
+  server.close(() => {
+    process.exit(1);
+  });
 });
