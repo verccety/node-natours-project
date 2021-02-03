97dd23c6648a07c0bce333409fe66688c942721b
Pavel <verccety1@gmail.com>
2021-02-03 22:22:57 +0700

    Generate JWT token when sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
 
+// eslint-disable-next-line import/prefer-default-export
 export const signup = catchAsync(async (request, response, next) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm,
+  });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   response.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
