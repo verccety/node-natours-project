a0fbfe3ffa0b806aed8e8425dfcab0d1a13c7e48
Pavel <verccety1@gmail.com>
2021-02-06 12:26:52 +0700

    Update user data route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 import User from '../models/userModel.js';
+import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
+
+const filterObject = (object, ...allowedFields) => {
+  const newObject = {};
+  Object.keys(object).forEach((element) => {
+    if (allowedFields.includes(element)) newObject[element] = object[element];
+  });
+
+  return newObject;
+};
 
 export const getAllUsers = catchAsync(async (request, response, next) => {
   const users = await User.find();
@@ -10,6 +20,36 @@ export const getAllUsers = catchAsync(async (request, response, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+export const updateMe = catchAsync(async (request, response, next) => {
+  // 1) Error if user posts a password
+  if (request.body.password || request.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates, please use  /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Filtered out unwanted fields
+  const filteredBody = filterObject(request.body, 'name', 'email'); //grab only two fields to prevent posting stuff like "role: admin", etc..
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(
+    request.user.id,
+    filteredBody,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  response.status(200).json({
+    status: 'sucess',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,8 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
 router.patch(
   '/updateMyPassword',
   authController.protect,
