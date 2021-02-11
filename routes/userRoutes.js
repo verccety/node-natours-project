47e816d673c48f0865196270ed51abc923b49f92
Pavel <verccety1@gmail.com>
2021-02-11 15:04:35 +0700

    Refactor auth a bit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,20 +9,17 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// Protect all routes below; since middleware runs in sequence
+router.use(authController.protect);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+router.get('/me', userController.getMe, userController.getUser);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
